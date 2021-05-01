a5d277a73278de49e892c0a87a8b0273c8139b86
Jincheng Kuang <jkuang10@myseneca.ca>
2021-05-01 00:09:07 -0400

    added q20

---
+++ cpp/20_Valid_Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+	bool isValid(string s) {
+		if (s.size() % 2 != 0) {
+			return false;
+		}
+		stack<char> st;
+		for (char c : s) {
+			if (c == '(' || c == '[' || c == '{') {
+				st.push(c);
+			}
+			else {
+				if (st.empty()) {
+					return false;
+				}
+				if (st.top() == '(' && c != ')' || st.top() == '[' && c != ']' || st.top() == '{' && c != '}') {
+					return false;
+				}
+				st.pop();
+			}
+		}
+		return st.empty();
+	}
+};
