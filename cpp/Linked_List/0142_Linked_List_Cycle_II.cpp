1dc8cef7a96cb8be0847f1b254cb33be69bcbc28
jay <jkuang10@myseneca.ca>
2022-02-21 10:59:58 -0500

    #142

---
+++ cpp/Linked_List/0142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *detectCycle(ListNode *head) {
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) {
+        slow = head;
+        while (slow != fast) {
+          slow = slow->next;
+          fast = fast->next;
+        }
+        return slow;
+      }
+    }
+    return nullptr;
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
