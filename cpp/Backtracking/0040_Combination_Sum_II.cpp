075273370a1eec37ec8d64c09477c61be4f34b92
jay <jkuang10@myseneca.ca>
2022-03-01 20:26:07 -0500

    #40

---
+++ cpp/Backtracking/0040_Combination_Sum_II.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+private:
+  vector<int> path;
+  vector <vector<int>> res;
+
+  void backtracking(vector<int>& candidates, int target, int sum, int start) {
+    if (sum == target) {
+      res.push_back(path);
+      return;
+    }
+    for (int i = start; i < candidates.size() && sum + candidates[i] <= target; ++i) {
+      if (i > start && candidates[i] == candidates[i - 1]) continue;
+      sum += candidates[i];
+      path.push_back(candidates[i]);
+      backtracking(candidates, target, sum, i + 1);
+      sum -= candidates[i];
+      path.pop_back();
+    }
+  }
+
+public:
+  vector <vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    backtracking(candidates, target, 0, 0);
+    return res;
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
