e959197e0c8feeede87a6e7811655fbe1aa63074
jay <jkuang10@myseneca.ca>
2022-03-07 14:48:37 -0500

    #452

--- cpp/Greedy/0452_Minimum_Number_of_Arrows_to_Burst_Balloons.cpp
+++ cpp/Greedy/0452_Minimum_Number_of_Arrows_to_Burst_Balloons.cpp
@@ -5,6 +5,9 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+  /*
+   * 1st solution sort by start
+   * */
   int findMinArrowShots(vector <vector<int>>& points) {
     if (points.size() == 0) return 0;
     auto cmp = [](const vector<int>& p1, const vector<int>& p2) {
@@ -15,6 +18,27 @@ public:
     for (int i = 1; i < points.size(); i++) {
       if (points[i][0] > points[i - 1][1]) res++;
       else points[i][1] = min(points[i - 1][1], points[i][1]);
+    }
+    return res;
+  }
+
+  /*
+   * 2nd solution sort by end
+   * */
+  int findMinArrowShots(vector <vector<int>>& points) {
+    if (points.size() == 0) return 0;
+    auto cmp = [](const vector<int>& p1, const vector<int>& p2) {
+      return p1[1] < p2[1];
+    };
+    sort(points.begin(), points.end(), cmp);
+    int res = 1;
+    int end = points[0][1];
+    for (int i = 1; i < points.size(); i++) {
+      int start = points[i][0];
+      if (start > end) {
+        res++;
+        end = points[i][1];
+      }
     }
     return res;
   }
