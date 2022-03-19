1a631a257dc37ebe7d144455b3dbfd9add419dae
jay <jkuang10@myseneca.ca>
2022-03-18 22:58:38 -0400

    #70

--- cpp/Dynamic_Programming/0070_Climbing_Stairs.cpp
+++ cpp/Dynamic_Programming/0070_Climbing_Stairs.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 using namespace std;
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -14,6 +16,19 @@ public:
       dp[2] = sum;
     }
     return dp[2];
+  }
+
+  int climbStairs2(int n) {
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 0; i <= n; ++i) {
+      for (int j = 0; j <= 2; ++j) {
+        if (i - j >= 0) {
+          dp[i] += dp[i - j];
+        }
+      }
+    }
+    return dp[n];
   }
 };
 //leetcode submit region end(Prohibit modification and deletion)
