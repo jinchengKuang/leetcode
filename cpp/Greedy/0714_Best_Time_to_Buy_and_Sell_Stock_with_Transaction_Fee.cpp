aaa2a6af22517d20cc1babc66405eabe1636713c
jay <jkuang10@myseneca.ca>
2022-03-22 23:13:32 -0400

    #714 dp

--- cpp/Greedy/0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
+++ cpp/Greedy/0714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
@@ -5,6 +5,9 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+  /*
+   * greedy
+   * */
   int maxProfit(vector<int>& prices, int fee) {
     int profit = 0;
     int minPrice = prices[0];
@@ -16,6 +19,34 @@ public:
       }
     }
     return profit;
+  }
+
+  /*
+   * dp 2d
+   * */
+  int maxProfit2(vector<int>& prices, int fee) {
+    // state: 0 = buy; 1 = sell;
+    vector <vector<int>> dp(prices.size(), vector<int>(2, 0));
+    dp[0][0] = -prices[0];
+    for (int i = 1; i < prices.size(); ++i) {
+      dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+      dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+    }
+    return max(dp[prices.size() - 1][0], dp[prices.size() - 1][1]);
+  }
+
+  /*
+   * dp 1d
+   * */
+  int maxProfit2(vector<int>& prices, int fee) {
+    // state: 0 = buy; 1 = sell;
+    vector<int> dp(2, 0);
+    dp[0] = -prices[0];
+    for (int i = 1; i < prices.size(); ++i) {
+      dp[0] = max(dp[0], dp[1] - prices[i]);
+      dp[1] = max(dp[1], dp[0] + prices[i] - fee);
+    }
+    return max(dp[0], dp[1]);
   }
 };
 //leetcode submit region end(Prohibit modification and deletion)
