4ab54559c9c85239bd48a4195b243f1e3ed6b0ed
jay <jkuang10@myseneca.ca>
2022-03-26 22:31:52 -0400

    #53 dp

--- cpp/Greedy/0053_Maximum_Subarray.cpp
+++ cpp/Greedy/0053_Maximum_Subarray.cpp
@@ -5,6 +5,9 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
+  /*
+   * greedy
+   * */
   int maxSubArray(vector<int>& nums) {
     int res = INT_MIN;
     int subArrSum = 0;
@@ -14,6 +17,18 @@ public:
       if (subArrSum < 0) subArrSum = 0;
     }
     return res;
+  }
+
+  /*
+   * dp
+   * */
+  int maxSubArray2(vector<int>& nums) {
+    vector<int> dp(nums.size(), 0);
+    dp[0] = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+      dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+    }
+    return *max_element(dp.begin(), dp.end());
   }
 };
 //leetcode submit region end(Prohibit modification and deletion)
