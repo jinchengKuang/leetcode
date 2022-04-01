984e93884213f58a0bbe7d9ab5966b2a31445533
jay <jkuang10@myseneca.ca>
2022-03-31 22:43:20 -0400

    #344

--- cpp/String/0344_Reverse_String.cpp
+++ cpp/String/0344_Reverse_String.cpp
@@ -8,22 +8,33 @@ public:
   /*
    * 1st solution recursion
    * */
-  void reverseString(vector<char> &s) {
+  void reverseString(vector<char>& s) {
     traversal(s, 0, s.size() - 1);
   }
 
-  void traversal(vector<char> &s, int left, int right) {
+  // recursion 3 steps:
+  // 1. parameters and  return value
+  // 2. termination condition
+  // 3. main logic for each recursion
+
+  void traversal(vector<char>& s, int left, int right) {
     if (right <= left) return;
-    swap(s[left++], s[right--]);
+    // more efficient way to swap(s[left++], s[right--]);
+    s[left] ^= s[right];
+    s[right] ^= s[left];
+    s[left] ^= s[right];
+    left++;
+    right--;
+
     traversal(s, left, right);
   }
 
   /*
    * 2nd solution two pointers
    * */
-  void reverseString2(vector<char> &s) {
-    for (int left = 0, right = s.size() - 1; left < s.size() / 2; ++left, --right) {
-//      swap(s[left], s[right]);
+  void reverseString2(vector<char>& s) {
+    for (int left = 0, right = s.size() - 1; left < s.size() >> 1; ++left, --right) {
+      // more efficient way to swap(s[left], s[right]);
       s[left] ^= s[right];
       s[right] ^= s[left];
       s[left] ^= s[right];
