9fc402bc2634d32d4f88b6a860e9427e1383d2e1
jay <jkuang10@myseneca.ca>
2022-03-31 22:28:38 -0400

    #704

--- cpp/Array/0704_Binary_Search.cpp
+++ cpp/Array/0704_Binary_Search.cpp
@@ -5,12 +5,34 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-  int search(vector<int> &nums, int target) {
+  /*
+ * Binary search
+ * note:
+ * 1. nums array is sorted
+ * 2. elements are unique
+ * 3. difference between [left, right] and [left, right)
+ * */
+
+  // [left, right]
+  int search(vector<int>& nums, int target) {
     int left = 0;
-    int right = nums.size() - 1;
+    int right = nums.size() - 1; // different, right is included
     while (left <= right) {
       int mid = left + ((right - left) >> 1);
-      if (nums[mid] > target) right = mid - 1;
+      if (nums[mid] > target) right = mid - 1; // different
+      else if (nums[mid] < target) left = mid + 1;
+      else return mid;
+    }
+    return -1;
+  }
+
+  // [left, right)
+  int search(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size(); // different, right is not included
+    while (right > left) {
+      int mid = left + ((right - left) >> 1);
+      if (nums[mid] > target) right = mid; // different
       else if (nums[mid] < target) left = mid + 1;
       else return mid;
     }
