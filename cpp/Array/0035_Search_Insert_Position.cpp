0c4f7a0f9a63f06ff76c7ebb2824fbed50f4b05d
jay <jkuang10@myseneca.ca>
2022-04-01 21:04:48 -0400

    #35

--- cpp/Array/0035_Search_Insert_Position.cpp
+++ cpp/Array/0035_Search_Insert_Position.cpp
@@ -5,13 +5,24 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-  int searchInsert(vector<int> &nums, int target) {
-    int left = 0, right = nums.size();
+  int searchInsert(vector<int>& nums, int target) {
+    int left = 0, right = nums.size(); // different
     while (left < right) {
       int mid = left + ((right - left) >> 1);
-      if (target < nums[mid]) right = mid;
-      else if (target > nums[mid]) left = mid + 1;
-      else right = mid;
+      if (target > nums[mid]) left = mid + 1;
+      else if (target < nums[mid]) right = mid; // different
+      else return mid;
+    }
+    return left;
+  }
+
+  int searchInsert(vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1; // different
+    while (left <= right) {
+      int mid = left + ((right - left) >> 1);
+      if (target > nums[mid]) left = mid + 1;
+      else if (target < nums[mid]) right = mid - 1; // different
+      else return mid;
     }
     return left;
   }
