4fc5217b9b296e6b72245089b36c803fbabb1ce6
jay <jkuang10@myseneca.ca>
2022-04-09 00:27:14 -0400

    #347

--- cpp/Stack_and_Queue/0347_Top_K_Frequent_Elements.cpp
+++ cpp/Stack_and_Queue/0347_Top_K_Frequent_Elements.cpp
@@ -7,34 +7,39 @@ using namespace std;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 public:
-  class mycomparison {
-  public:
-    bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
-      return lhs.second > rhs.second;
-    }
-  };
-
+  /*
+   * use hash map to count the elements
+   * use min heap to keep the top Kth element in the heap
+   * */
   vector<int> topKFrequent(vector<int>& nums, int k) {
+    // map: key->element's value; value->element's count
     unordered_map<int, int> map;
-    for (int i = 0; i < nums.size(); i++) {
+    for (int i = 0; i < nums.size(); ++i) {
       map[nums[i]]++;
     }
 
-    priority_queue < pair < int, int >, vector < pair < int, int >>, mycomparison > pri_que;
-
-    for (auto it = map.begin(); it != map.end(); it++) {
-      pri_que.push(*it);
-      if (pri_que.size() > k) {
-        pri_que.pop();
+    // customize min heap comparator (> is min heap)
+    struct myComparison {
+      bool operator()(pair<int, int>& p1, pair<int, int>& p2) {
+        return p1.second > p2.second;
       }
+    };
+
+    // heap
+    priority_queue < pair < int, int >, vector < pair < int, int >>, myComparison > pq;
+
+    // push into pq, if size of pq is larger than k, pop the top i.e. min
+    for (auto& p: map) {
+      pq.push(p);
+      if (pq.size() > k) pq.pop();
     }
 
-    vector<int> res;
-    while (!pri_que.empty()) {
-      res.push_back((pri_que.top().first));
-      pri_que.pop();
+    vector<int> result;
+    while (!pq.empty()) {
+      result.push_back(pq.top().first);
+      pq.pop();
     }
-    return res;
+    return result;
   }
 };
 //leetcode submit region end(Prohibit modification and deletion)
