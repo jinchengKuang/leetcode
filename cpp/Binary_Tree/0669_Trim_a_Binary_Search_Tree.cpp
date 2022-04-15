bbef59fee717608da6b687be527c33f01a4a6d26
jay <jkuang10@myseneca.ca>
2022-04-14 23:20:32 -0400

    #669

---
+++ cpp/Binary_Tree/0669_Trim_a_Binary_Search_Tree.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  /*
+   * 1st recursive
+   * binary search tree: for each node, left is smaller, right is larger
+   * if node->left->val < low, then all node->left-left are abandoned
+   * same to node->right->val > high
+   * if within [low, high], then use DFS to recursively search the tree
+   * */
+  TreeNode* trimBST(TreeNode* root, int low, int high) {
+    if (!root) return nullptr;
+    if (root->val < low) return trimBST(root->right, low, high);
+    if (root->val > high) return trimBST(root->left, low, high);
+    root->left = trimBST(root->left, low, high);
+    root->right = trimBST(root->right, low, high);
+    return root;
+  }
+};
+//leetcode submit region end(Prohibit modification and deletion)
