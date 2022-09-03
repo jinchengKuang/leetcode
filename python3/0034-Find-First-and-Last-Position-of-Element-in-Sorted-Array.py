2fff1fa652e1056beab746b4aa6ad648b44619e2
jay <jkuang10@myseneca.ca>
2022-09-03 00:06:12 -0400

    #34

--- python3/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
+++ python3/0034-Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
@@ -3,4 +3,34 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        pass
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        leftbound = left
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        rightbound = right
+        return [leftbound, rightbound] if leftbound <= rightbound else [-1, -1]
+
+
+s = Solution()
+nums = [5, 7, 7, 8, 8, 10]
+target = 8
+print(s.searchRange(nums, target))
+
+target = 6
+print(s.searchRange(nums, target))
+
+nums = []
+target = 0
+print(s.searchRange(nums, target))
