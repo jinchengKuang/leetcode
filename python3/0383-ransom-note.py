0c3f91a46839778fa67d9d78ebb4feef9b9a186f
jay <jkuang10@myseneca.ca>
2022-10-04 23:54:06 -0400

    #383

--- python3/0383-ransom-note.py
+++ python3/0383-ransom-note.py
@@ -1,14 +1,30 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        record = [0] * 26
-        for c in magazine:
-            record[ord(c) - ord('a')] += 1
-        for c in ransomNote:
-            if record[ord(c) - ord('a')] == 0:
-                return False
+        record = dict()
+        for ch in magazine:
+            if ch in record:
+                record[ch] += 1
             else:
-                record[ord(c) - ord('a')] -= 1
+                record[ch] = 1
+        for ch in ransomNote:
+            if ch in record:
+                if record[ch] == 0:
+                    return False
+                else:
+                    record[ch] -= 1
+            else:
+                return False
         return True
+
+        # record = [0] * 26
+        # for c in magazine:
+        #     record[ord(c) - ord('a')] += 1
+        # for c in ransomNote:
+        #     if record[ord(c) - ord('a')] == 0:
+        #         return False
+        #     else:
+        #         record[ord(c) - ord('a')] -= 1
+        # return True
 
 
 ransomNote = "a"
