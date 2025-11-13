57cfe3adb6d85518ba8214b92f0e7bf357741861
Jincheng Kuang <jkuang37@gatech.edu>
2025-11-12 20:10:05 -0500

    #724 v2

--- python3/0724-find-pivot-index.py
+++ python3/0724-find-pivot-index.py
@@ -1,3 +1,4 @@
+# v1
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         left_preSum = [0] * (len(nums) + 1)
@@ -12,4 +13,17 @@ class Solution:
         for i in range(1, len(left_preSum)):
             if left_preSum[i] == right_preSum[i - 1]:
                 return i - 1
+        return -1
+
+# v2
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left_sum, right_sum = 0, sum(nums)
+
+        for i in range(len(nums)):
+            right_sum -= nums[i]
+            if right_sum == left_sum:
+                return i
+            left_sum += nums[i]
+
         return -1
