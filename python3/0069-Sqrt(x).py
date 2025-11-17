6521f99fd4343534923eddc1acb93626be25a899
Jincheng Kuang <jkuang37@gatech.edu>
2025-11-16 20:16:41 -0500

    #0069

--- python3/0069-Sqrt(x).py
+++ python3/0069-Sqrt(x).py
@@ -5,16 +5,16 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x <= 1:
             return x
-        left, right = 1, x // 2
+
+        left, right = 0, x // 2
         while left <= right:
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2
             mid_square = mid * mid
             if mid_square > x:
                 right = mid - 1
-            elif mid_square < x:
-                left = mid + 1
             else:
-                return mid
+                left = mid + 1
+
         return left if left * left <= x else left - 1
 
 
